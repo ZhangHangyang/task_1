20a8f9e30b98eb545a3530149032d0ae810c9baa
ZhangHangyang <100186440+ZhangHangyang@users.noreply.github.com>
2022-04-12 14:41:54 +0800

    Add files via upload

---
+++ lab1/src/stack.h
@@ -0,0 +1,20 @@
+#pragma once
+class stack {
+private:
+	int Size = 0;
+	int* data;
+	int* top;
+	int i = 0;
+	int m = 0;
+public:
+	stack(int size);
+	stack();
+	~stack();
+	void push(int data);
+	int* front();
+	void pop();
+	int ContainedNumber();
+	int size();
+	int getStack(int number);
+};
+
